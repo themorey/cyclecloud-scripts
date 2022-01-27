85ae3ba410232a732f6925da4b33bf1c60347e66
Jerry Morey <themorey@gmail.com>
2022-01-27 09:24:25 -0500

    Add files via upload

---
+++ slurm-install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+
+export VER=20.11.7-1
+export ccSlurmVer=2.4.7
+
+mkdir -p /opt/cycle/jetpack/system/chef/cache/jetpack/downloads
+
+# Create slurm and munge users if needed (11100 and 11101 are CycleCloud default values)
+if ! grep -q "slurm" /etc/passwd; then
+    useradd -u 11100 --no-create-home slurm
+fi
+if ! grep -q "munge" /etc/passwd; then
+    useradd -u 11101 --no-create-home munge
+fi
+
+yum install -y epel-release
+yum install -y munge
+
+# Create a declarative array to index the slurm rpms
+declare -a slurmrpms
+slurmrpms=( "slurm" "slurm-devel" "slurm-example-configs" "slurm-slurmctld" "slurm-slurmd" "slurm-perlapi" "slurm-torque" "slurm-openlava" )
+
+# Loop through the RPMs in the array to download and install each
+for rpm in "${slurmrpms[@]}"; do
+    # Download RPM from CycleCloud Slurm repo
+    wget -o /tmp/${rpm}-${VER}.el7.x86_64.rpm  https://github.com/Azure/cyclecloud-slurm/releases/download/${ccSlurmVer}/${rpm}-${VER}.el7.x86_64.rpm
+    
+    # install RPM from CycleCloud Slurm repo
+    yum install -y /tmp/${rpm}-${VER}.el7.x86_64.rpm
+    
+    # touch the filename in /opt/cycle/jetpack/system/chef/cache/jetpack/downloads so Cycle will not re-install it
+    touch /opt/cycle/jetpack/system/chef/cache/jetpack/downloads/${rpm}-${VER}.el7.x86_64.rpm
+done
+
+# Determine OS version for pluginName
+if grep -q 'VERSION_ID="8"' /etc/os-release; then
+    pluginName="job_submit_cyclecloud_centos8_${VER}.so"
+  else
+    pluginName="job_submit_cyclecloud_centos_${VER}.so"
+fi
+
+# Install Job Submit Plugin for CycleCloud
+wget -O /usr/lib64/slurm/job_submit_cyclecloud.so  https://github.com/Azure/cyclecloud-slurm/releases/download/${ccSlurmVer}/${pluginName}
+touch /etc/cyclecloud-job-submit.installed
+
+rm -rf /tmp/slurm*.rpm*
