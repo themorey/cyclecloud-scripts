9cbe688df5ffa4ee4fab4ba65838497c5642c1dc
Jerry Morey <themorey@gmail.com>
2022-09-08 12:09:28 -0400

    check for slurm.hpc
    added a condition to only run if the Slurm partition is marked as hpc (slurm.hpc = True)

--- slurm-beeond/slurm-epilog-beeond.sh
+++ slurm-beeond/slurm-epilog-beeond.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -x
 
-nodefile=/shared/home/$SLURM_JOB_USER/nodefile-$SLURM_JOB_ID
-if [ -e $nodefile ] ; then
+if [ "$(/opt/cycle/jetpack/bin/jetpack slurm.hpc)" == "True" ]; then
+  nodefile=/shared/home/$SLURM_JOB_USER/nodefile-$SLURM_JOB_ID
+  if [ -e $nodefile ] ; then
 
-  logdir="/sched/log"
-  logfile=$logdir/slurm_epilog.log
-  exec 1>$logfile 2>&1
+    logdir="/sched/log"
+    logfile=$logdir/slurm_epilog.log
+    exec 1>$logfile 2>&1
 
-  # Workaround Beeond stop umount issue
-  while read host; do
-    sudo -u $SLURM_JOB_USER ssh -o UserKnownHostsFile=/dev/null StricHostKeyChecking=no -t $host 'sudo umount -l /mnt/beeond && df -h |grep beeond '
-  done < $nodefile
+    # Workaround Beeond stop umount issue
+    while read host; do
+      sudo -u $SLURM_JOB_USER ssh -t $host 'sudo umount -l /mnt/beeond && df -h |grep beeond '
+    done < $nodefile
 
-  echo "$(date).... Stopping beeond"
-  sudo -u $SLURM_JOB_USER beeond stop -n $nodefile -L -d -P -c
+    echo "$(date).... Stopping beeond"
+    sudo -u $SLURM_JOB_USER beeond stop -n $nodefile -L -d -P -c
 
-  #rm $nodefile
+    # Workaround Beeond stop umount issue
+    while read host; do
+      sudo -u $SLURM_JOB_USER ssh -t $host 'sudo umount -l /mnt/beeond && df -h |grep beeond '
+    done < $nodefile
 
+    #rm $nodefile
+  fi
 fi
